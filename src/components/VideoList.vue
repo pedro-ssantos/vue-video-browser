ad57cd92988df2544b1906fe592ffc3db2f36bd1
Pedro Santos <peusantos@gmail.com>
2021-02-27 09:57:42 -0300

    First steps.

---
+++ src/components/VideoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    VideoList
+    <VideoListItem
+      v-for="video in videos"
+      v-bind:video="video"
+      :key="video.etag"
+    ></VideoListItem>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem'
+
+export default {
+  name: 'VideoList',
+  props: { videos: Array },
+  components: { VideoListItem }
+}
+</script>
+
+<style></style>
